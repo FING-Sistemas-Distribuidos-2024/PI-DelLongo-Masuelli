ae7256a6a7bbde5bf444adb0a1e2806cf15c4790
micakil <micaeladellongo@gmail.com>
2024-06-05 10:58:09 -0300

    Update IPs and indent

--- redis_client/src/App.js
+++ redis_client/src/App.js
@@ -1,50 +1,54 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [receivedMessage, setReceivedMessage] = useState('');
+	const [message, setMessage] = useState('');
+	const [receivedMessage, setReceivedMessage] = useState('');
 
-  const handleSendMessage = async () => {
-    try {
-      // ip del backend
-      const response = await axios.post('http://10.230.50.4:5000/send', { message });
-      alert(response.data.status);
-    } catch (error) {
-      console.error('Error sending message:', error);
-      alert('Error sending message.');
-    }
-  };
+	const handleSendMessage = async () => {
+		try {
+			// ip del backend
+			const response = await axios.post('http://10.230.50.4:5000/send', {message});
+			alert(response.data.status);
+		} catch (error) {
+			console.error('Error sending message:', error);
+			alert('Error sending message.');
+		}
+	};
 
-  const handleReceiveMessage = async () => {
-    try {
-      const response = await axios.get('http://10.230.50.4:5000/receive');
-      setReceivedMessage(response.data.message);
-    } catch (error) {
-      console.error('Error receiving message:', error);
-      alert('Error receiving message.');
-    }
-  };
+	const handleReceiveMessage = async () => {
+		try {
+			const response = await axios.get('http://10.230.50.4:5000/receive');
+			setReceivedMessage(response.data.message);
+		} catch (error) {
+			console.error('Error receiving message:', error);
+			alert('Error receiving message.');
+		}
+	};
 
-  return (
-    <div className="App">
-      <h1>Redis Messaging App</h1>
-      <div>
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Enter your message"
-        />
-        <button onClick={handleSendMessage}>Send Message</button>
-      </div>
-      <div>
-        <button onClick={handleReceiveMessage}>Receive Message</button>
-        <p>Received Message: {receivedMessage}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Redis Messaging App</h1>
+			<div>
+				<input
+					type="text"
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+					placeholder="Enter your message"
+				/>
+				<button onClick={handleSendMessage}>Send Message</button>
+			</div>
+			<div>
+				<button onClick={handleReceiveMessage}>Receive Message</button>
+				<textarea
+					value={`Received Message: ${receivedMessage}`}
+					readOnly
+					style={{width: '100%', height: '200px'}}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- redis_server/server.js
+++ redis_server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');  // permite hacer endpoint de http-rest 
 const bodyParser = require('body-parser');
 const cors = require('cors');  // cross origin resources 
-const { createClient } = require('redis');
+const {createClient} = require('redis');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,13 +9,13 @@ app.use(cors()); // Habilitar CORS
 
 // cliente de Redis
 const client = createClient({
-  // ip del servidor de redis
-  url: 'redis://10.230.50.2:6379'
-  //url: 'redis://localhost:6379'
+	// ip del servidor de redis
+	url: 'redis://10.230.50.2:6379'
+	//url: 'redis://localhost:6379'
 });
 
 client.on('error', (err) => {
-  console.error('Error de Redis:', err);
+	console.error('Redis error:', err);
 });
 
 // conectar el cliente de Redis
@@ -23,28 +23,28 @@ client.connect().catch(console.error);
 
 // ruta para enviar mensajes
 app.post('/send', async (req, res) => {
-  const message = req.body.message;
-  try {
-    await client.lPush('message', message);
-    res.json({ status: 'Mensaje enviado a Redis.' });
-  } catch (err) {
-    console.error('Error al enviar el mensaje a Redis:', err);
-    res.status(500).json({ status: 'Error al enviar el mensaje a Redis.' });
-  }
+	const message = req.body.message;
+	try {
+		await client.lPush('message', message);
+		res.json({status: 'Message sent to Redis.'});
+	} catch (err) {
+		console.error('Error sending message to Redis:', err);
+		res.status(500).json({status: 'Error sending message to Redis.'});
+	}
 });
 
 // ruta para recibir mensajes
 app.get('/receive', async (req, res) => {
-  try {
-    const message = await client.rPop('message');
-    res.json({ message });
-  } catch (err) {
-    console.error('Error al recibir el mensaje de Redis:', err);
-    res.status(500).json({ status: 'Error al recibir el mensaje de Redis.' });
-  }
+	try {
+		const message = await client.rPop('message');
+		res.json({message});
+	} catch (err) {
+		console.error('Error receiving message from Redis:', err);
+		res.status(500).json({status: 'Error receiving message from Redis.'});
+	}
 });
 
 // iniciar el servidor en el puerto 5000
 app.listen(5000, () => {
-  console.log('Servidor corriendo en el puerto 5000.');
+	console.log('Server running at 5000.');
 });
